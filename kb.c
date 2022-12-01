42163a65366b6a1824cc2cc4ee43c7ee3c3b74c4
kit <kit@debianjr.res6.spectrum.com>
2022-12-01 18:47:27 -0500

    .

--- kb.c
+++ kb.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <string.h>
+#include <time.h>
 
 char *KB_Keymash (void) {
 	const char samples [] =
@@ -34,11 +35,21 @@ char *KB_Keymash (void) {
 	const size_t len = sizeof (samples) / sizeof (*samples);
 	
 	return strndup (
-		(size_t) (rand () * ((float) RAND_MAX / (float) len)) + (char *) samples, // pos
-		(size_t) ((rand () * (RAND_MAX / (len - 8))) + 8) // len, minimum is 8 chrs
+		(size_t) ((float) rand () * ((float) len / (float) RAND_MAX)) + (char *) samples, // pos
+		rand () * ((float) 64 / (float) RAND_MAX) + 8 // len, minimum is 8 chrs
 	);
 }
 
+char *KB_KeymashFmt (const char *fmt) {
+	char *mash = KB_Keymash ();
+	char *out = malloc ((strlen (fmt) - 1) + strlen (mash));
+	
+	sprintf (out, fmt, mash);
+	free (mash);
+	return out;
+}
+
 void main (void) {
+	srand (time (NULL));
 	puts (KB_Keymash ());
 }
